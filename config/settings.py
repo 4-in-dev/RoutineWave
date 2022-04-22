632118b5ec9a4a0aadb931ab40aca6fb78e9fa60
KIMHOONHEE <nmdkims@gmail.com>
2022-04-22 23:27:13 +0900

    수정 - 장고 RDS, S3연결후 CSS 적용 안되는 문제 해결, Collect static #1

--- config/settings.py
+++ config/settings.py
@@ -118,14 +118,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = 'static/'
 
-# setting
+
+# setting local
 STATIC_URL = "/static/"
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR
 ]
+
+# setting eb
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
