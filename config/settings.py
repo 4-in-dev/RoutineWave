dd518b7d3711f9292fc32fc103c6f4fff62554ac
KIMHOONHEE <nmdkims@gmail.com>
2022-05-20 13:13:15 +0900

    Feat : 수정 - 배포 설정 수정완료 #12

--- config/settings.py
+++ config/settings.py
@@ -10,14 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 
 from .my_settings import (MY_DATABASES, MY_SECRET, MY_SECRET_ACCESS_KEY,
                           S3_BUCKET_NAME)
-
-from datetime import timedelta
-
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
